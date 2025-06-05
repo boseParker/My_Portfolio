8e0bd4bcf2dd3fd8c5349f81163626bab02c5105
Bose <bosem3708@gmail.com>
2025-06-05 12:56:46 +0530

    resume Update

--- src/constants.js
+++ src/constants.js
@@ -15,7 +15,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/boseParker",
   resume:
-    "https://drive.google.com/file/d/1xmhiwfffNZWi_anrP_BgSCehuqyloRRZ/view?usp=drive_open",
+    "https://drive.google.com/file/d/11peWKJu4mbj-DRt5a3s3CH5F8cb85xyF/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/bose-m-96473433a/",
 
   insta: "https://www.instagram.com/bose.m66/",
